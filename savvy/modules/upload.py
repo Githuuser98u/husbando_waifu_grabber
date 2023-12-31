0f79422c87f12fe3a2d11eea3ce8462c08e508ed
Githuuser98u <103206449+Githuuser98u@users.noreply.github.com>
2023-12-31 13:34:23 +0530

    Update upload.py

--- savvy/modules/upload.py
+++ savvy/modules/upload.py
@@ -12,7 +12,7 @@ img_url character-name anime-name rarity-number
 
 use rarity number accordingly rarity Map
 
-rarity_map = 1 (⚪️ Common), 2 (🟣 Rare) , 3 (🟡 Legendary), 4 (🟢 Medium)"""
+rarity_map = 1 (🍃 Common), 2 (☘️ Rare) , 3 (🍀 Legendary), 4 (🌿 Medium)"""
 
 
 
@@ -48,7 +48,7 @@ async def upload(update: Update, context: CallbackContext) -> None:
             await update.message.reply_text('Invalid URL.')
             return
 
-        rarity_map = {1: "⚪ Common", 2: "🟣 Rare", 3: "🟡 Legendary", 4: "🟢 Medium"}
+        rarity_map = {1: "🍃 Common", 2: "☘️ Rare", 3: "🍀 Legendary", 4: "🌿 Medium"}
         try:
             rarity = rarity_map[int(args[3])]
         except KeyError:
@@ -132,7 +132,7 @@ async def update(update: Update, context: CallbackContext) -> None:
         if args[1] in ['name', 'anime']:
             new_value = args[2].replace('-', ' ').title()
         elif args[1] == 'rarity':
-            rarity_map = {1: "⚪ Common", 2: "🟣 Rare", 3: "🟡 Legendary", 4: "🟢 Medium", 5: "💮 Special edition"}
+            rarity_map = {1: "🍃 Common", 2: "☘️ Rare", 3: "🍀 Legendary", 4: "🌿 Medium", 5: "🍁 Special edition"}
             try:
                 new_value = rarity_map[int(args[2])]
             except KeyError:
